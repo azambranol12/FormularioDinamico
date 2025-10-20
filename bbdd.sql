dab3a7b2bb2a0115b6ead41f1d40e5793a3f3465
azambranol12 <adrianzambranolopez.guadalupe@alumnado.fundacionloyola.net>
2025-10-20 18:31:27 +0200

    Update bbdd.sql

--- bbdd.sql
+++ bbdd.sql
@@ -1,25 +1,25 @@
 
 --Y las opciones son la información de los selects y checkboxs
-CREATE TABLE opcionesContacto (
+CREATE TABLE opcionesContacto(
     id smallint unsigned AUTO_INCREMENT PRIMARY KEY,
     valor varchar(100) NOT NULL,
     etiqueta varchar(100) NOT NULL
 );
 
-CREATE TABLE opcionesEleccion (
+CREATE TABLE opcionesEleccion(
     id smallint unsigned AUTO_INCREMENT PRIMARY KEY,
     texto varchar(100) NOT NULL
 );
 
 --Esta es la información que le he añadido a las tablas
 -- Opciones de contacto
-INSERT INTO opcionesContacto (valor, etiqueta) VALUES
+INSERT INTO opcionesContacto(valor, etiqueta) VALUES
 ('email', 'Correo electrónico'),
 ('telefono', 'Teléfono'),
 ('whatsApp', 'WhatsApp');
 
 -- Opciones de disponibilidad (select)
-INSERT INTO opcionesEleccion (texto) VALUES
+INSERT INTO opcionesEleccion(texto) VALUES
 ('Mañanas'),
 ('Tardes'),
 ('Mediodía');
