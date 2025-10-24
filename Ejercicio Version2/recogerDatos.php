35eb5994b2c42010eff9f0ce5272b1446337427a
azambranol12 <adrianzambranolopez.guadalupe@alumnado.fundacionloyola.net>
2025-10-24 23:03:55 +0200

    a
    a

--- Ejercicio Version2/recogerDatos.php
+++ Ejercicio Version2/recogerDatos.php
@@ -18,7 +18,7 @@
     $comentario = isset($_POST["comentario"]) ? trim( $_POST["comentario"]) : '';
     $contacto = isset($_POST['contacto']) ? $_POST['contacto'] : [];
 
-
+//Validaciones para ver si esta vacío la variable eso añadi a esta version, cosa que en la otra no
     if($nombre == '' || $apellido == '' || $contrasenia == '' || $accion == ''|| $eleccion == '')
     {
         echo '<h2><a href="index.php"> Algo en blanco, vuelve a introducir todos los datos. </a></h2>';
@@ -35,7 +35,8 @@
         echo '<h2><a href="index.php"> Selecciona Horario. </a></h2>';
         exit();
     }
-    if($comentario == '')
+
+    /*if($comentario == '') Insertar el somentario en null estoy probando
     {
         $sql_Insertar = "INSERT INTO usuarios (nombre, apellido, contrasenia, accion, horarioId) VALUES ('$nombre', '$apellido', '$contrasenia', '$accion','$eleccion');";
         echo $sql_Insertar.'</br>';
@@ -44,10 +45,7 @@
         $sql_Insertar = "INSERT INTO usuarios (nombre, apellido, contrasenia, accion, horarioId, comentario) VALUES ('$nombre', '$apellido', '$contrasenia', '$accion','$eleccion','$comentario');";
         echo $sql_Insertar.'</br>';
         $resultadoContacto = $conexion->query($sql_Insertar);
-    }
-
-
-   
+    }*/
 
 
     $usuarioId = $conexion->insert_id;
